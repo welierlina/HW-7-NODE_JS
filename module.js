d1f6660a014459c8234b938dca9d6a9346550cd8
unknown <welierlina08@gmail.com>
2024-02-24 16:56:50 +0700

    file module.js

---
+++ module.js
@@ -0,0 +1,15 @@
+exports.luas = function (s) {
+    return s * s
+}
+
+exports.keliling = function (s) {
+    return 4 * s
+}
+
+exports.luasP = function (p, l) {
+    return p * l
+}
+
+exports.kelilingP = function (p, l) {
+    return (2*p) + (2*l)
+}
